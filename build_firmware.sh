8d328313ad36336a179628a5da634ea7b5808594
Biswa96 <nathbappai@gmail.com>
2020-02-11 21:38:34 +0530

    move download commands to shell script

--- build_firmware.sh
+++ build_firmware.sh
@@ -1,9 +1,26 @@
 ﻿#
+
+# Download required boot files
+
+LINK="https://github.com/raspberrypi/firmware/raw/master/boot"
+wget $LINK/fixup4.dat
+wget $LINK/start4.elf
+wget $LINK/bcm2711-rpi-4-b.dtb
+wget $LINK/overlays/miniuart-bt.dtbo
+mkdir overlays
+mv miniuart-bt.dtbo overlays
+mkdir Firmware
+
+# Compile base tools and prepare environment
+
 make -C edk2/BaseTools || exit 1
 export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
 export WORKSPACE=$PWD
 export PACKAGES_PATH=$WORKSPACE/edk2:$WORKSPACE/edk2-platforms:$WORKSPACE/edk2-non-osi
 source edk2/edksetup.sh || exit 1
+
+# Build the EFI files
+
 build -a AARCH64 -t GCC5 -p edk2-platforms/Platform/RaspberryPi/RPi4/RPi4.dsc -b DEBUG --pcd gEfiMdeModulePkgTokenSpaceGuid.PcdFirmwareVendor=L"https://github.com/pftf/RPi4" --pcd gEfiMdeModulePkgTokenSpaceGuid.PcdFirmwareVersionString=L"UEFI Firmware $APPVEYOR_REPO_TAG_NAME (DEBUG)" || exit 1
 cp Build/RPi4/DEBUG_GCC5/FV/RPI_EFI.fd Firmware/RPI_M4D.fd
 build -a AARCH64 -t GCC5 -p edk2-platforms/Platform/RaspberryPi/RPi4/RPi4.dsc -b DEBUG -D ACPI_BASIC_MODE_ENABLE=1 --pcd gEfiMdeModulePkgTokenSpaceGuid.PcdFirmwareVendor=L"https://github.com/pftf/RPi4" --pcd gEfiMdeModulePkgTokenSpaceGuid.PcdFirmwareVersionString=L"UEFI Firmware $APPVEYOR_REPO_TAG_NAME (DEBUG)" || exit 1
